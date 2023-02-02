badf51dfaa55881a465ae462ebb583622a0ef6a7
chandu <chandushine70@gmail.com>
2023-02-02 12:49:38 +0530

    added

---
+++ terraform-ansible/resource.tf
@@ -0,0 +1,34 @@
+resource "aws_instance" "node1" {
+  ami           = "ami-00874d747dde814fa"
+  instance_type = "t2.micro"
+  key_name      = "mylaptop"
+  tags = {
+    Name = "ACN"
+  }
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/id_rsa")
+      host        = aws_instance.node1.public_ip
+    }
+
+    inline = [
+      "sudo apt update",
+      "sudo apt install software-properties-common",
+      "sudo add-apt-repository --yes --update ppa:ansible/ansible",
+      "sudo apt install ansible -y",
+      "ansible --version",
+      "git clone https://github.com/chandushine/demo.git",
+      "cd demo/database/jenkins-terraform-ansible",
+      "ansible-playbook -i hosts mongodb.yml"
+    ]
+  }
+}
+
+
+
+
+
+
+
